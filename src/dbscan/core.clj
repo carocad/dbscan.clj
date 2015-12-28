05344ecc0a2f4075582ae8a8cbb4f02443f8466e
Camilo Roca <carocad@unal.edu.co>
2015-12-28 13:23:20 -0500

    first prototype with basic working functionality

--- src/dbscan/core.clj
+++ src/dbscan/core.clj
@@ -1,6 +1,7 @@
 (ns dbscan.core
-  (:require [clojure.core.matrix :refer [compute-matrix get-row
-                                         row-count distance]] :reload))
+  (:require [clojure.set :as cset]
+            [clojure.core.matrix :refer [compute-matrix get-row row-count distance]]
+            :reload))
 
 ; the return includes the target point itself
 (defn- region-query
@@ -19,11 +20,25 @@
 
 (defn- not-clustered?
   [clusters [index _]]
-  (not-any? (fn [cluster] (cluster index) clusters)))
+  (not-any? (fn [cluster] (cluster index)) clusters))
 
 (defn- enough-neighbors?
-  [neighborhood minpts]
-  (> (count neighborhood) minpts))
+  [[index neighborhood] minpts]
+  (if (> (count neighborhood) minpts)
+    index
+    nil))
+
+(defn- cluster
+  [seed minpts unclassified]
+  (loop [neighbors (into (hash-set) (unclassified seed))
+         edge      neighbors]
+    (let [far-neighbors     (map #(unclassified %) edge)
+          good-neighboor    (filter #(> (count %) minpts) far-neighbors)
+          unique-neighbors  (into (hash-set) (apply concat good-neighboor))
+          new-neighbors     (filter #(not (neighbors %)) unique-neighbors)]
+      (if (empty? new-neighbors)
+        neighbors
+        (recur (cset/union neighbors new-neighbors) new-neighbors)))))
 
 ; data is a sequence of vectors with index as first value and a vector as
 ; second value
@@ -34,25 +49,19 @@
   ([data eps minpts dist-fn]
    (DBSCAN data eps minpts dist-fn (make-query-fn data dist-fn)))
   ([data eps minpts dist-fn query-fn]
-   (loop [clusters     (transient [])
-          unclassified (into (hash-map) (map (fn [[index _]] [index (query-fn index eps)])
+   (loop [clusters     []
+          unclassified (into (hash-map) (map (fn [[index _]] [index (query-fn eps index)])
                                              data))
-          seed         (some #(enough-neighbors? % eps) unclassified)]
+          seed         (some #(enough-neighbors? % minpts) unclassified)]
      (if (nil? seed)
-       (persistent! clusters)   ;make a set with the unclassified points = noise
-       (let [new-clusters      (conj! clusters (cluster seed minpts unclassified query-fn eps))
-             still-unclassified (into {} (filter #(not-clustered? new-clusters %) unclassified))
+       [clusters (map first unclassified)]   ; TODO: make a set with the unclassified points = noise
+       (let [new-clusters       (conj clusters (cluster seed minpts unclassified))
+             still-unclassified (into (hash-map) (filter #(not-clustered? new-clusters %) unclassified))
              new-seed           (some #(enough-neighbors? % minpts) still-unclassified)]
          (recur new-clusters still-unclassified new-seed))))))
 
-(defn- cluster
-  [seed minpts unclassified query-region eps]
-  (loop [neighbors (query-region seed eps)
-         edge      neighbors]
-    (let [far-neighbors     (map #(query-region eps %) edge)
-          good-neighboor    (filter #(> (count %) minpts) far-neighbors)
-          unique-neighbors  (into (hash-set) (apply concat good-neighboor))
-          new-neighbors     (filter #(not (neighbors %)) unique-neighbors)]
-      (if (empty? new-neighbors)
-        (persistent! neighbors)
-        (recur (conj! neighbors new-neighbors) new-neighbors)))))
+; NOTE: I think it is easier to make data be simple sequence of value vectors
+; such that the indexing part is handled internally
+
+(DBSCAN '([0 [1 2 3]] [1 [1 2 3]] [2 [1 2 3]] [3 [1 2 3]] [4 [0 0 0]] [5 [0 0 0]] [6 [0 0 0]]
+          [7 [9 9 9]]) 1 2)
