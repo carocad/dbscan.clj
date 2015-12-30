38773d3f5518b7fbe02bcf5bf3b7e971e9311595
Camilo Roca <carocad@unal.edu.co>
2015-12-30 18:07:23 -0500

    incanter not as dependency

--- project.clj
+++ project.clj
@@ -7,8 +7,9 @@
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
                                   [org.clojure/test.check "0.9.0"]
-                                  [incanter/incanter-charts "1.5.5"]
-                                  [incanter/incanter-core "1.5.5"]]}})
+                                  ;[incanter/incanter-charts "1.5.5"]
+                                  ;[incanter/incanter-core "1.5.5"]
+                                  ]}})
 
 (comment
   incanter-core and incanter-charts are included mainly to visualize the
